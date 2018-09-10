945559c3871bd600dd99586caed1f9e952a263ad
Luke <lukejbass@gmail.com>
2018-09-09 19:09:24 -0700

    Template

---
+++ a1/RedditAverage.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class RedditAverage extends Configured implements Tool {
+
+	public static class TokenizerMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+		private final static LongWritable one = new LongWritable(1);
+		private Text word = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String[] words = value.toString().split("[\\p{Punct}\\s]+");
+
+			for (String str : words) {
+				if (str.length() == 0) {
+					continue;
+				}
+
+				word.set(str.toLowerCase());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new RedditAverage(), args);
+		System.exit(res);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = this.getConf();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(RedditAverage.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(LongSumReducer.class);
+		job.setReducerClass(LongSumReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+}
