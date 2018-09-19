7c2f1a7ac94d105c4461e23041eb865baf98abdc
Luke <lukejbass@gmail.com>
2018-09-19 14:37:45 -0700

    Part 3

---
+++ a2/p3/wikipedia_popular.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+import sys, operator
+
+inputs = sys.argv[1]
+output = sys.argv[2]
+
+conf = SparkConf().setAppName('wikipedia popular')
+sc = SparkContext(conf=conf)
+assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
+assert sc.version >= '2.3'  # make sure we have Spark 2.3+
+
+def words_once(line):
+    for w in line.split():
+        yield (w.lower(), 1)
+
+def get_key(kv):
+    return kv[0]
+
+def output_format(kv):
+    k, v = kv
+    return '%s %i' % (k, v)
+
+text = sc.textFile(inputs)
+words = text.flatMap(words_once)
+wordcount = words.reduceByKey(operator.add)
+
+outdata = wordcount.sortBy(get_key).map(output_format)
+outdata.saveAsTextFile(output)
