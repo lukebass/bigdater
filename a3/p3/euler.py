d2c1b680a9067ca0054be8c8a5f2c78b79b6bec9
Luke <lukejbass@gmail.com>
2018-09-25 16:29:44 -0700

    Almost there

--- a3/p3/euler.py
+++ a3/p3/euler.py
@@ -2,23 +2,28 @@ from pyspark import SparkConf, SparkContext
 import sys, operator, random
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 
-def main(samples):
-    totalIterations = sc.range(0, samples).map(generateE).reduce(operator.add)
+def main(samples, numSlices):
+    totalIterations = sc.range(0, samples, numSlices=numSlices).mapPartitions(getIterations).sum()
     print(totalIterations/samples)
 
-def generateE(sample):
+def getIterations(partition):
     random.seed()
-    sum = 0.0
     itr = 0
-    while sum < 1:
-        sum += random.random()
-        itr += 1
-    
-    return itr
+
+    for x in partition:
+        sum = 0.0
+        while sum < 1:
+            sum += random.random()
+            itr += 1
+
+    return [itr]
 
 if __name__ == '__main__':
     conf = SparkConf().setAppName('example code')
     sc = SparkContext(conf=conf)
     assert sc.version >= '2.3'  # make sure we have Spark 2.3+
-    samples = int(sys.argv[1])
-    main(samples)
+
+    numSlices = 4
+    if (sys.argv[2]):
+        numSlices = int(sys.argv[2])
+    main(int(sys.argv[1]), numSlices)
