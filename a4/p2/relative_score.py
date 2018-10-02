779048e2611fd81d332686a29eb9c76027629a34
Luke <lukejbass@gmail.com>
2018-10-02 12:18:23 -0700

    Part 2

--- a4/p2/relative_score.py
+++ a4/p2/relative_score.py
@@ -3,15 +3,10 @@ import sys, json
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 
 def main(inputs, output):
-    text = sc.textFile(inputs)
-    averages = text.map(getSubreddits).reduceByKey(combineComments).map(getAverage).filter(lambda x: x[1] > 0)
-    
-    outdata = averages.sortBy(getKey).map(json.dumps)
-    outdata.saveAsTextFile(output)
-
-def getSubreddits(line):
-    comment = json.loads(line)
-    return (comment['subreddit'], (1, comment['score']))
+    text = sc.textFile(inputs).map(json.loads).cache()
+    averages = text.map(lambda x: (x['subreddit'], (1, x['score']))).reduceByKey(combineComments).map(getAverage).filter(lambda x: x[1] > 0)
+    comments = text.map(lambda x: (x['subreddit'], x)).join(averages).map(getRelativeScore)
+    comments.sortBy(lambda x: x[0], False).map(json.dumps).saveAsTextFile(output)
 
 def combineComments(accum, comment):
     return (accum[0] + comment[0], accum[1] + comment[1])
@@ -20,8 +15,9 @@ def getAverage(kv):
     k, v = kv
     return (k, v[1]/v[0])
 
-def getKey(subreddit):
-    return subreddit[0]
+def getRelativeScore(kv):
+    k, v = kv
+    return (v[0]['score']/v[1], v[0]['author'])
 
 if __name__ == '__main__':
     conf = SparkConf().setAppName('reddit averages')
