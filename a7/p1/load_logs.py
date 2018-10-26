7956d1de7f3566652fcfee95ae1435194140baa5
Luke <lukejbass@gmail.com>
2018-10-26 16:08:33 -0700

    Finish

--- a7/p1/load_logs.py
+++ a7/p1/load_logs.py
@@ -1,6 +1,6 @@
 from cassandra.cluster import Cluster
 from cassandra.query import BatchStatement
-import sys, re, os, gzip, datetime
+import sys, re, os, gzip, datetime, uuid
 assert sys.version_info >= (3, 5)
 
 wordsep = re.compile(r'^(\S+) - - \[(\S+) [+-]\d+\] \"[A-Z]+ (\S+) HTTP/\d\.\d\" \d+ (\d+)$')
@@ -9,7 +9,7 @@ def main(inputs, keyspace, table):
     cluster = Cluster(['199.60.17.188', '199.60.17.216'])
     session = cluster.connect(keyspace)
     batch = BatchStatement()
-    insertLog = session.prepare("INSERT INTO " + table + " (id, host, datetime, path, bytes) VALUES (uuid(), ?, ?, ?, ?)")
+    insertLog = session.prepare("INSERT INTO " + table + " (id, host, datetime, path, bytes) VALUES (?, ?, ?, ?, ?)")
 
     count = 0
     for f in os.listdir(inputs):
@@ -18,7 +18,7 @@ def main(inputs, keyspace, table):
                 fields = getFields(line)
                 
                 if fields:
-                    batch.add(insertLog, (fields[0], fields[1], fields[2], fields[3]))
+                    batch.add(insertLog, (fields[0], fields[1], fields[2], fields[3], fields[4]))
                     count += 1
                 
                 if count > 300:
@@ -32,7 +32,7 @@ def main(inputs, keyspace, table):
 def getFields(line):
     fields = wordsep.split(line)
     if (len(fields) > 3):
-        return (fields[1], datetime.datetime.strptime(fields[2], '%d/%b/%Y:%H:%M:%S'), fields[3], int(fields[4]))
+        return (uuid.uuid4(), fields[1], datetime.datetime.strptime(fields[2], '%d/%b/%Y:%H:%M:%S'), fields[3], int(fields[4]))
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
--- a7/p2/load_logs_spark.py
+++ a7/p2/load_logs_spark.py
@@ -1,15 +1,14 @@
 from pyspark.sql import SparkSession, functions, types, Row
-import sys, re, datetime
+import sys, re, datetime, uuid
 assert sys.version_info >= (3, 5)
-cluster_seeds = ['199.60.17.188', '199.60.17.216']
-spark = SparkSession.builder.appName('nasa logs').config('spark.cassandra.connection.host', ','.join(cluster_seeds)).getOrCreate()
+spark = SparkSession.builder.appName('nasa logs').config('spark.cassandra.connection.host', '199.60.17.188, 199.60.17.216').getOrCreate()
 assert spark.version >= '2.3'
 
 wordsep = re.compile(r'^(\S+) - - \[(\S+) [+-]\d+\] \"[A-Z]+ (\S+) HTTP/\d\.\d\" \d+ (\d+)$')
 
 def main(inputs, keyspace, table):
     schema = types.StructType([
-        types.StructField('id', types.DoubleType(), True),
+        types.StructField('id', types.StringType(), True),
         types.StructField('host', types.StringType(), True),
         types.StructField('datetime', types.DateType(), True),
         types.StructField('path', types.StringType(), True),
@@ -23,7 +22,7 @@ def main(inputs, keyspace, table):
 def getFields(line):
     fields = wordsep.split(line)
     if (len(fields) > 3):
-        yield Row(host=fields[1], datetime=datetime.datetime.strptime(fields[2], '%d/%b/%Y:%H:%M:%S'), path=fields[3], bytes=int(fields[4]))
+        yield (str(uuid.uuid4()), fields[1], datetime.datetime.strptime(fields[2], '%d/%b/%Y:%H:%M:%S'), fields[3], int(fields[4]))
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
